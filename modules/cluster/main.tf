67add8eb85427e9d2986b937802287a244b9d7c7
dlia <david.lia@melitaltd.com>
2022-01-12 22:30:42 +0100

    Fix: setting remove_default_node_pool to false

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -23,7 +23,7 @@ resource "google_container_cluster" "jx_cluster" {
   enable_kubernetes_alpha  = var.enable_kubernetes_alpha
   enable_legacy_abac       = var.enable_legacy_abac
   enable_shielded_nodes    = var.enable_shielded_nodes
-  remove_default_node_pool = true
+  remove_default_node_pool = false //----set by david----
   initial_node_count       = var.min_node_count
   logging_service          = var.logging_service
   monitoring_service       = var.monitoring_service
