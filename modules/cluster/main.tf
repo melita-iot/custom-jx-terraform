dad63ba2d2deae14d3807c261b69b61325ecd933
dlia <david.lia@melitaltd.com>
2022-01-13 13:16:14 +0100

    Chore: testing with different master_ipv4_cidr_block value

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -42,7 +42,7 @@ resource "google_container_cluster" "jx_cluster" {
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
+    master_ipv4_cidr_block  = "172.17.0.0/28"
   }
 
   //-----------------------
